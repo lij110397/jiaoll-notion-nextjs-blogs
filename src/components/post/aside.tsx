83fb6bca53c01bff2e6233cb5317f94b4ae5b46e
lij110397 <lij110397@163.com>
2024-05-29 19:30:51 +0800

    adjust eslint

--- src/components/post/aside.tsx
+++ src/components/post/aside.tsx
@@ -1,39 +1,40 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+// import React, { useEffect } from 'react'
+import React from 'react'
+// import { useState } from 'react'
 import TableOfContent from '@/components/table-of-content'
 import { PostProps } from '@/types/types'
-import { FaArrowCircleUp } from 'react-icons/fa'
+// import { FaArrowCircleUp } from 'react-icons/fa'
 // aside component will present two elements when scrolling
 // 1. table of content component to show catalog of the post
 // 2. arrow up button to help user to scroll to the top of the page
 
 const Aside = ({ metadata, blockMap }: PostProps) => {
   // add scroll event listener to show the arrow up button and toc components
-  const [showScrollElement, setShowScrollElement] = useState(false)
+  // const [showScrollElement, setShowScrollElement] = useState(false)
 
   // useEffect event add event listener to window scroll
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 100) {
-        setShowScrollElement(true)
-      } else {
-        setShowScrollElement(false)
-      }
-    }
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if (window.scrollY > 100) {
+  //       setShowScrollElement(true)
+  //     } else {
+  //       setShowScrollElement(false)
+  //     }
+  //   }
 
-    window.addEventListener('scroll', handleScroll)
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [metadata, blockMap])
+  //   window.addEventListener('scroll', handleScroll)
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll)
+  //   }
+  // }, [metadata, blockMap])
 
   // scroll event for arrow up to the top button
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    })
-  }
+  // const scrollToTop = () => {
+  //   window.scrollTo({
+  //     top: 0,
+  //     behavior: 'smooth',
+  //   })
+  // }
 
   return (
     <aside>
