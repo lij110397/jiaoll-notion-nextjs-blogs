07c3a7deb6f8fa413297f0227affec493123dfae
lij110397 <lij110397@163.com>
2024-05-31 15:10:11 +0800

    change loadpath [ci skip]

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -6,11 +6,11 @@ import { initReactI18next } from 'react-i18next';
 import HttpApi from 'i18next-http-backend';
 // import LanguageDetector from 'i18next-browser-languagedetector';
 
-console.log('LANGUAGE_LOAD_PATH:', process.env.NEXT_PUBLIC_LANGUAGE_LOAD_PATH );
+// console.log('LANGUAGE_LOAD_PATH:', process.env.NEXT_PUBLIC_LANGUAGE_LOAD_PATH );
 const languageLoadPath = process.env.NODE_ENV === 'production' 
     ? process.env.NEXT_PUBLIC_LANGUAGE_LOAD_PATH 
     : 'http://localhost:3000/locales/{{lng}}/{{ns}}.json';
-console.log('languageLoadPath:', languageLoadPath);
+// console.log('languageLoadPath:', languageLoadPath);
 i18n
   .use(HttpApi) // 加载翻译文件
   // .use(LanguageDetector) // 自动检测语言
